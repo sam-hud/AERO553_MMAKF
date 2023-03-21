d78e7180daafa8c0cbc82a7a38fee444e3904555
jfrabosi <97116626+jfrabosi@users.noreply.github.com>
2023-03-20 18:56:14 -0700

    updated motor stuff

--- src/main.cpp
+++ src/main.cpp
@@ -11,44 +11,44 @@
 // TODO: Check pins
 
 // Motor pins
-#define MOTOR_PWM 12
-#define MOTOR_DIR 13
-#define MOTOR_BRK 14
+#define PWM_PIN 13
+#define DIR_PIN 12
+#define BRK_PIN 34
+#define ENA_PIN 35
+#define ENB_PIN 32
+
+// Acoustic pins
+#define REC_R 25
+#define REC_L 33
+#define TRANS 26
 
 // Input pins
-#define LIM_PIN 2
+#define LIM_PIN 14
 #define SLIDER_PIN 36
-#define ACCEL_PIN 4
-
-// Arduino Serial Setup
-#define RXD2 16
-#define TXD2 17
-HardwareSerial Arduino(2); // Using UART2: RX = 16, TX = 17
-
-// TODO: Accelerometer Setup
+#define ACCEL_PIN 0
 
 // OLED
 #define OLED_SDA 21
 #define OLED_SCL 22
+#define OLED_RST 16
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
 
 /* Define Shares*/
 Queue<float> motorSpeed(1, "Motor Speed");
 Share<bool> limitSwitchPressed("Limit Switch Pressed");
-Share<float> sliderPosition("Slider Position");
+Share<uint16_t> sliderPosition("Slider Position");
 Share<float> actualMotorPosition("Actual Motor Position");
 Share<float> KF1MotorPosition("KF1 Motor Position");
 Share<float> KF2MotorPosition("KF2 Motor Position");
 Share<float> KF3MotorPosition("KF3 Motor Position");
 Share<float> MMAEMotorPosition("MMAE Motor Position");
 Share<float> arduinoReading("Arduino Reading");
-Share<String> arduinoString("Arduino String Reading");
 Share<float> accelerometerReading("Accelerometer Reading");
 
 // Create each motor driver object
-Motor motor(MOTOR_PWM, MOTOR_DIR, MOTOR_BRK);
+Motor motor(PWM_PIN, DIR_PIN, BRK_PIN);
 
 // Create Kalman Filters
 KalmanFilter KF1;
@@ -66,8 +66,13 @@ void motorTask(void *p_params)
   while (true)
   {
     // TODO: set motor speed based on incoming queue data
-    motor.setSpeed(motorSpeed.get());
+    motor.setSpeed(127);
   }
+}
+
+void encoderTask(void *p_params)
+{
+  
 }
 
 //********************************************************************************
@@ -75,7 +80,6 @@ void motorTask(void *p_params)
 void displayTask(void *p_params)
 {
   uint8_t displayState = 0; // Set start case to 0
-  Serial.println("Display Task Started");
   while (true)
   {
     // TODO: set task period based on incoming queue data
@@ -84,45 +88,36 @@ void displayTask(void *p_params)
     display.setTextColor(WHITE);
 
     display.setCursor(0, 0);
-    display.print("Slider:");
-
-    display.setCursor(0, 20);
+    display.println("MMAKF");
     display.setTextSize(1);
-    // display.println(arduinoString.get());
-    display.println(sliderPosition.get());
 
-    // display.setCursor(0, 0);
-    // display.print("MMAE KF");
-    // display.setTextSize(1);
+    display.setCursor(10, 20);
+    display.print("Actual:");
+    display.setCursor(60, 20);
+    display.print(actualMotorPosition.get());
 
-    // display.setCursor(10, 20);
-    // display.print("Actual:");
-    // display.setCursor(60, 20);
-    // display.print(actualMotorPosition.get());
+    display.setCursor(10, 30);
+    display.print("KF 1:");
+    display.setCursor(60, 30);
+    display.print(KF1MotorPosition.get());
 
-    // display.setCursor(10, 30);
-    // display.print("KF 1:");
-    // display.setCursor(60, 30);
-    // display.print(KF1MotorPosition.get());
+    display.setCursor(10, 40);
+    display.print("KF 2:");
+    display.setCursor(60, 40);
+    display.print(KF2MotorPosition.get());
 
-    // display.setCursor(10, 40);
-    // display.print("KF 2:");
-    // display.setCursor(60, 40);
-    // display.print(KF2MotorPosition.get());
+    display.setCursor(10, 50);
+    display.print("KF 3:");
+    display.setCursor(60, 50);
+    display.print(KF3MotorPosition.get());
 
-    // display.setCursor(10, 50);
-    // display.print("KF 3:");
-    // display.setCursor(60, 50);
-    // display.print(KF3MotorPosition.get());
-
-    // display.setCursor(10, 60);
-    // display.print("MMAKF: ");
-    // display.setCursor(60, 60);
-    // display.print(MMAEMotorPosition.get());
+    display.setCursor(10, 60);
+    display.print("MMAKF: ");
+    display.setCursor(60, 60);
+    display.print(MMAEMotorPosition.get());
 
     display.display();
-    Serial.println("Display cycle");
-    vTaskDelay(10); // Task period
+    vTaskDelay(100); // Task period
   }
 }
 //********************************************************************************
@@ -132,16 +127,16 @@ void controlInputTask(void *p_params)
   while (true)
   {
     // TODO: read slider input and set shared variable
-    float sliderValue = (analogRead(SLIDER_PIN) / 3200.0F) * 150;
-    // Serial.println(sliderValue);
+    uint16_t sliderValue = analogRead(SLIDER_PIN);
     sliderPosition.put(sliderValue);
-    vTaskDelay(10); // Task period
+    vTaskDelay(100); // Task period
   }
 }
 //********************************************************************************
 // Accelerometer reading task
 void accelerometerTask(void *p_params)
 {
+  uint8_t accelState = 0; // Set start case to 0
   while (true)
   {
     // TODO: read accelerometer and set shared variable
@@ -152,22 +147,18 @@ void accelerometerTask(void *p_params)
 }
 //********************************************************************************
 // Arduino communication task
-void arduinoTask(void *p_params)
-{
-  while (true)
-  {
-    // Serial.println(Arduino.readString());
-    // arduinoReading.put(digitalRead(16));
-    // Serial.println(digitalRead(16));
-    while (Arduino.available())
-    {
-      String arduinoRead = Arduino.readString();
-      Serial.println(arduinoRead);
-      // arduinoReading.put(arduinoRead.toFloat());
-      arduinoString.put(arduinoRead);
-    }
-  }
-}
+// void arduinoTask(void *p_params)
+// {
+//   while (true)
+//   {
+//     while (Arduino.available())
+//     {
+//       String arduinoRead = Arduino.readString();
+//       arduinoReading.put(arduinoRead.toFloat());
+//       // Task period controlled by Arduino reading
+//     }
+//   }
+// }
 //********************************************************************************
 // Limit switch task
 void limitSwitchTask(void *p_params)
@@ -303,34 +294,37 @@ void setup()
   {
   } // Wait for port to be ready before continuing
 
-  Arduino.begin(115200, SERIAL_8N1, RXD2, TXD2); // Begin Arduino serial
-  arduinoReading.put(0);                         // Initialize shared variable
-  arduinoString.put("0");                        // Initialize shared variable
-  sliderPosition.put(0);                         // Initialize shared variable
-
   // Setup pins
-  pinMode(MOTOR_PWM, OUTPUT);
-  pinMode(MOTOR_DIR, OUTPUT);
-  pinMode(MOTOR_BRK, OUTPUT);
+  pinMode(PWM_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(BRK_PIN, OUTPUT);
+  pinMode(ENA_PIN, INPUT_PULLUP);
+  pinMode(ENB_PIN, INPUT_PULLUP);
   pinMode(LIM_PIN, INPUT);
   pinMode(SLIDER_PIN, INPUT);
+
+  // Reset OLED
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
 
   // Initialize OLED
   Wire.begin(OLED_SDA, OLED_SCL);
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false))
   {
     Serial.println(F("OLED not found"));
-    while (true)
-    {
-    };
+    while (1)
+      ;
   }
 
   // Start FreeRTOS tasks
   xTaskCreate(displayTask, "Display Task", 10000, NULL, 2, NULL);
   xTaskCreate(controlInputTask, "Control input Task", 4096, NULL, 3, NULL);
-  // xTaskCreate(accelerometerTask, "Accelerometer Task", 4096, NULL, 3, NULL);
+  xTaskCreate(accelerometerTask, "Accelerometer Task", 4096, NULL, 3, NULL);
+  xTaskCreate(motorTask, "Motor Task", 4096, NULL, 3, NULL);
   // xTaskCreate(arduinoTask, "Arduino Communication Task", 4096, NULL, 3, NULL);
-  // xTaskCreate(limitSwitchTask, "Limit Switch Task", 4096, NULL, 3, NULL);
+  xTaskCreate(limitSwitchTask, "Limit Switch Task", 4096, NULL, 3, NULL);
   // xTaskCreate(KF1Task, "KF1 Task", 10000, NULL, 3, NULL);
   // xTaskCreate(KF2Task, "KF2 Task", 10000, NULL, 3, NULL);
   // xTaskCreate(KF3Task, "KF3 Task", 10000, NULL, 3, NULL);
